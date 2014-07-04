b6ccafd67cd521f423c63382b9667e627bb7291c
Thomas Merkel <tm@core.io>
2014-07-04 16:33:24 +0200

    Create munin config by ourself because includedir didn't work on munin-node

--- copy/var/zoneinit/includes/40-munin-node.sh
+++ copy/var/zoneinit/includes/40-munin-node.sh
@@ -1,7 +1,2 @@
-# Add config folder for parsing additional config files
-echo "
-# Parse extra config files
-includedir /opt/local/etc/munin/munin-conf.d" >> /opt/local/etc/munin/munin-node.conf
-
 # Enable munin service
 /usr/sbin/svcadm enable svc:/pkgsrc/munin-node:default
