861d2d0ffaf5b5e76dbb0c854d38ceae9061ab4a
Thomas Merkel <tm@core.io>
2014-07-04 17:00:41 +0200

    configure munin plugins and add community plugins

--- copy/var/zoneinit/includes/40-munin-node.sh
+++ copy/var/zoneinit/includes/40-munin-node.sh
@@ -1,2 +1,18 @@
+# Enable plugins
+MUNIN_PLUGINS=(
+	'cpu'
+	'df'
+	'dovecot'
+	'iostat'
+	'load'
+	'uptime'
+	'users'
+	'vmstat'
+)
+
+for plugins in "${MUNIN_PLUGINS[@]}"; do 
+	ln -s /opt/local/lib/munin/plugins/${plugin} /opt/local/etc/munin/plugins/${plugin}
+done
+
 # Enable munin service
 /usr/sbin/svcadm enable svc:/pkgsrc/munin-node:default
