3e0a34ef9654e3f0a1c157bc8c1d7ed75e26c913
Thomas Merkel <tm@core.io>
2014-07-06 16:55:51 +0200

    Use mdata extra include folder

---
+++ copy/opt/core/var/mdata-setup/includes/31-dovecot.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Configure dovecot ssl certificates
+
+mdata-get mbox_ssl > /opt/local/etc/dovecot/ssl/dovecot.pem
+chmod 400 /opt/local/etc/dovecot/ssl/dovecot.pem
+
+if mdata-get mbox_ssl 1>/dev/null 2>&1; then
+	cat > /opt/local/etc/dovecot/ssl.conf <<EOF
+ssl = yes
+disable_plaintext_auth = yes
+ssl_cert = </opt/local/etc/dovecot/ssl/dovecot.pem
+ssl_key = </opt/local/etc/dovecot/ssl/dovecot.pem
+EOF
+fi
