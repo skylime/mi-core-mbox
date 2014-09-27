3807befcfc2df97c0f337aca9fbc96e0d1f79b09
Thomas Merkel <tm@core.io>
2014-09-27 20:03:00 +0200

    chg: dev: fix path for ssl config file

--- copy/opt/core/var/mdata-setup/includes/31-dovecot.sh
+++ copy/opt/core/var/mdata-setup/includes/31-dovecot.sh
@@ -5,7 +5,7 @@ mdata-get mbox_ssl > /opt/local/etc/dovecot/ssl/dovecot.pem
 chmod 400 /opt/local/etc/dovecot/ssl/dovecot.pem
 
 if mdata-get mbox_ssl 1>/dev/null 2>&1; then
-	cat > /opt/local/etc/dovecot/ssl.conf <<EOF
+	cat > /opt/local/etc/dovecot/conf.d/ssl.conf <<EOF
 ssl = yes
 disable_plaintext_auth = yes
 ssl_cert = </opt/local/etc/dovecot/ssl/dovecot.pem
