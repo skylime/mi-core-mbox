0f96959e6b7158ddacd0c5e64041ae78b5e36973
Thomas Merkel <tm@core.io>
2014-10-13 20:02:37 +0200

    add extra logfile for all other stuff

--- copy/var/zoneinit/includes/30-dovecot.sh
+++ copy/var/zoneinit/includes/30-dovecot.sh
@@ -1,5 +1,6 @@
 /usr/sbin/svcadm enable svc:/pkgsrc/dovecot:default
 
 # configure logadm
+logadm -w /var/log/dovecot/main -p 1d -C 10 -N -o dovecot -g dovenull -m 640 -c
 logadm -w /var/log/dovecot/info -p 1d -C 10 -N -o dovecot -g dovenull -m 640 -c
 logadm -w /var/log/dovecot/debug -p 1d -C 10 -N -o dovecot -g dovenull -m 640 -c
