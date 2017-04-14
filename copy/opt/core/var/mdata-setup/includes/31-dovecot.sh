25a776e3527e45533853216e7f3199e317558f04
Thomas Merkel <tm@core.io>
2017-04-14 18:01:09 +0200

    Version bump and Let's Encrypt support if no SSL cert is provided via mdata

--- copy/opt/core/var/mdata-setup/includes/31-dovecot.sh
+++ copy/opt/core/var/mdata-setup/includes/31-dovecot.sh
@@ -1,21 +1,8 @@
 #!/bin/bash
-# Configure dovecot ssl certificates
-
-mdata-get mbox_ssl > /opt/local/etc/dovecot/ssl/dovecot.pem
-chmod 400 /opt/local/etc/dovecot/ssl/dovecot.pem
-
-if mdata-get mbox_ssl 1>/dev/null 2>&1; then
-	cat > /opt/local/etc/dovecot/conf.d/core-ssl.conf <<EOF
-ssl = required
-disable_plaintext_auth = yes
-ssl_cert = </opt/local/etc/dovecot/ssl/dovecot.pem
-ssl_key = </opt/local/etc/dovecot/ssl/dovecot.pem
-EOF
-fi
+# Configure dovecot trusted proxies
 
 if mdata-get trusted_proxies 1>/dev/null 2>&1; then
 	cat > /opt/local/etc/dovecot/conf.d/core-proxy-trusted.conf <<EOF
 login_trusted_networks = $(mdata-get trusted_proxies)
 EOF
-
 fi
