13a4accba144656dec7710ea82bc727d51e0b8ca
Thomas Merkel <tm@core.io>
2018-03-11 12:29:19 +0100

    Move dovecot maildir home to extra folder in user home directory
    To avoid issues with dovcot user home folder and user maildir it's
    required to move the maildir to an extra subfolder in the user home.
    
    A migration script is provided in /opt/core/bin/dovecot-mv-maildir-home.sh

---
+++ copy/opt/core/bin/dovecot-mv-maildir-home.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+ls -d /var/mail/*/* | while read mail_home; do
+	if [[ ! -d "${mail_home}/maildir" ]]; then
+		cd "${mail_home}" && \
+			mkdir -p maildir && \
+				chown dovecot:dovecot maildir && \
+					mv !(.dovecot.lda-dupes|sieve|maildir|\.|\.\.) maildir/
+	fi
+done
