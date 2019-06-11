c89e43965703c100900106f9034e36019ae53afa
Thomas Merkel <tm@core.io>
2019-06-11 20:38:47 +0200

    Move away from mdata-setup to regular zoneinit

--- copy/opt/core/var/mdata-setup/includes/31-dovecot.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Configure dovecot trusted proxies
-
-if mdata-get trusted_proxies 1>/dev/null 2>&1; then
-	cat > /opt/local/etc/dovecot/conf.d/core-proxy-trusted.conf <<EOF
-login_trusted_networks = $(mdata-get trusted_proxies)
-EOF
-fi
