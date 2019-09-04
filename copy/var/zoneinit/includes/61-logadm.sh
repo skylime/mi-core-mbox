0f0272e2bf2df177b220c7c0a0c35ff37d6a3bfd
Thomas Merkel <tm@core.io>
2019-09-04 21:28:59 +0200

    Fix logadm for dovecot default group

--- copy/var/zoneinit/includes/61-logadm.sh
+++ copy/var/zoneinit/includes/61-logadm.sh
@@ -2,6 +2,6 @@
 
 logadm -w '/var/log/redis/redis.log'  -A 7d -p 1d -c -N -o redis -g redis -m 640
 
-logadm -w '/var/log/dovecot/main'     -A 7d -p 1d -c -N -o dovecot -g dovecotnull -m 640
-logadm -w '/var/log/dovecot/info'     -A 7d -p 1d -c -N -o dovecot -g dovecotnull -m 640
-logadm -w '/var/log/dovecot/debug'    -A 7d -p 1d -c -N -o dovecot -g dovecotnull -m 640
+logadm -w '/var/log/dovecot/main'     -A 7d -p 1d -c -N -o dovecot -g dovenull -m 640
+logadm -w '/var/log/dovecot/info'     -A 7d -p 1d -c -N -o dovecot -g dovenull -m 640
+logadm -w '/var/log/dovecot/debug'    -A 7d -p 1d -c -N -o dovecot -g dovenull -m 640
